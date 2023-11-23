f97b2ad5cfc0404e06b8318ef8c1c95ae542ad65
Abir Hossen Ornob <abirhosain090@gmail.com>
2023-11-23 21:42:49 +0600

    Preserved scroll

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,10 +1,19 @@
 <template>
     <div>Users</div>
     <Nav></Nav>
+
+    <div class="mt-[1000px] flex gap-5">
+        <p>the Current time is {{ time }}</p>
+        <Link href="/users" class="text-blue-600" preserve-scroll>Refresh</link>
+    </div>
 </template>
 
 <script setup>
 import Nav from "../Shared/Nav.vue";
+import { Link } from "@inertiajs/vue3";
+defineProps({
+    time: String,
+});
 </script>
 
 <style lang="scss" scoped></style>
