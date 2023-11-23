b83a8668feba1b359f995ff3f82d3d4aa0462ebf
Abir Hossen Ornob <abirhosain090@gmail.com>
2023-11-23 20:36:57 +0600

    inertia progress bar implemented

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,14 @@
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 
+//for progress bar
+import NProgress from "nprogress";
+import { router } from "@inertiajs/vue3";
+
+router.on("start", () => NProgress.start());
+router.on("finish", () => NProgress.done());
+// end progress
+
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
@@ -10,5 +18,11 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .mount(el);
+    },
+    progress: {
+        delay: 250,
+        color: "#29d",
+        includeCSS: true,
+        showSpinner: true,
     },
 });
